c2e58bcdea631420423226a84477b9fa271d6299
sgalle16 <sgalle16@eafit.edu.co>
2023-08-03 22:30:20 -0500

    adding about page

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-
 
 def home(request):
     # return HttpResponse("<h1>Welcome to the Home Page</h1>")
     return render(request, 'home.html', {'name': 'Santiago Gallego'})
+
+def about(request):
+    return render(request, 'about.html',)
