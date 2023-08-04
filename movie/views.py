3d52157aaac4a5334eabb52710f03a5db16e9ae4
sgalle16 <sgalle16@eafit.edu.co>
2023-08-03 22:47:54 -0500

    adding about and name color

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     # return HttpResponse("<h1>Welcome to the Home Page</h1>")
-    return render(request, 'home.html', {'name': 'Santiago Gallego'})
+    return render(request, 'home.html',)
+
 
 def about(request):
     return render(request, 'about.html',)
