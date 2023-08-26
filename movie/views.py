7794afc6ad203da057dead76be8ac9a6f89f056b
sgalle16 <sgalle16@eafit.edu.co>
2023-08-25 19:52:38 -0500

    list of movies

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 
 def home(request):
     # return HttpResponse("<h1>Welcome to the Home Page</h1>")
-    return render(request, 'home.html',)
+    # return render(request, 'home.html',)
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies})
 
 
 def about(request):
     return render(request, 'about.html',)
-
